ec345124527743563c7f5eee043fd987d92fdcbf
Chathusha Mendis <chathushamendis@icloud.com>
2023-08-05 07:54:16 +0530

    user input

--- code/game.py
+++ code/game.py
@@ -23,7 +23,8 @@ class Game:
 
 		# timer 
 		self.timers = {
-			'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+			'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+			'horizontal move': Timer(MOVE_WAIT_TIME)
 		}
 		self.timers['vertical move'].activate()
 
@@ -49,6 +50,7 @@ class Game:
 	def run(self):
 
 		# update
+		self.input()
 		self.timer_update()
 		self.sprites.update()
 
@@ -59,6 +61,17 @@ class Game:
 		self.draw_grid()
 		self.display_surface.blit(self.surface, (PADDING,PADDING))
 		pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if not self.timers['horizontal move'].active:
+			if keys[pygame.K_LEFT]:
+				self.tetromino.move_horizontal(-1)
+				self.timers['horizontal move'].activate()
+			if keys[pygame.K_RIGHT]:
+				self.tetromino.move_horizontal(1)
+				self.timers['horizontal move'].activate()
 
 class Tetromino:
 	def __init__(self, shape, group):
@@ -73,6 +86,10 @@ class Tetromino:
 	def move_down(self):
 		for block in self.blocks:
 			block.pos.y += 1
+
+	def move_horizontal(self, amount):
+		for block in self.blocks:
+			block.pos.x += amount
 
 class Block(pygame.sprite.Sprite):
 	def __init__(self, group, pos, color):
